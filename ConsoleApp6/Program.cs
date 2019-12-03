45077cc209715b7344fd03355aa8c423501cf626
Jack <Jacekgoog@Gmail.com>
2019-12-03 16:16:23 +0100

    First steps

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ConsoleApp6
 {
@@ -7,8 +9,64 @@ namespace ConsoleApp6
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Is het gelukt??");
-            Console.WriteLine("Is het gelukt?????");
+        }
+
+    }
+
+    public class ChoiceQuestion
+    {
+        public List<string> choices;
+
+        public ChoiceQuestion()
+        {
+
+            choices = new List<string>();
+        }
+
+        public void addChoice(String choice, Boolean correct)
+        {
+            
+            choices.Add(choice);
+            
+            if (correct)
+            {
+                String choiceString = "" + choices.Count;
+                setAnswer(choiceString);
+            }
+        }
+
+        public void display()
+        {
+            display();
+            for(int i= 0; i <choices.Count; i++)
+            {
+             
+                int choiceNumber = i + 1;
+                Console.WriteLine($"{choiceNumber}: {choices.ElementAt(i)}");
+            }
+        }
+
+        public class Question
+        {
+            public String text;
+            public String answer;
+
+            public Question()
+            {
+                text = "";
+                answer = "";
+            }
+
+            public void setText(String questionText)
+            {
+                text = questionText;
+            }
+
+            public void setAnswer(String correctResponse)
+            {
+
+            }
         }
     }
+
 }
